47f35d85201da29ca77dceb684b0136276b1a10f
Jackson Hardaker <jackson@jacksonhardaker.com>
2017-07-26 21:41:46 -0400

    Add clipboard copy func

--- src/bg/background.js
+++ src/bg/background.js
@@ -7,7 +7,23 @@
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
+  function (request, sender, sendResponse) {
+    chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+  /**
+   * Copies the given text to the clipboard
+   */
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message && message.type == 'copy') {
+    var input = document.createElement('textarea');
+    document.body.appendChild(input);
+    input.value = message.text;
+    input.focus();
+    input.select();
+    var successful = document.execCommand('Copy');
+    alert('Copying text command was ' + (successful ? 'successful' : 'unsuccessful'));
+    input.remove();
+  }
+});
