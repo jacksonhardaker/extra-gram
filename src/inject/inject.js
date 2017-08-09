6b9500d27310bf31bf2e98493970d1a56272ce56
Jackson Hardaker <jackson@jacksonhardaker.com>
2017-08-09 14:05:43 -0400

    Add function to get meta data

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,18 +4,23 @@ chrome.extension.sendMessage({}, function (response) {
 			clearInterval(readyStateCheckInterval);
 
 			(function () {
-				
+
 				const MODAL_INDEX = {
 					followers: 1,
 					following: 2
 				};
 				const INSTAGRAM_CLASS_NAMES = {
-					userListItem : '_6e4x5', // li element inside modal
+					userListItem: '_6e4x5', // li element inside modal
 					userName: '_2g7d5', // link element inside modal
 					metaItems: '_t98z6', // link element
 					closeButton: '_dcj9f', // link element
 					commentTextArea: '_bilrf', // Textarea element
-					upvoteButton: '_eszkz' // link element
+					upvoteButton: '_eszkz', // link element
+					postDateTime: '_p29ma', // <time> element
+					postCommentListItem: '_ezgzd', // first <li> element
+					postLikes: '_nzn1h', // <span> element
+					postDiv: '_mck9w', // <div> with nested link
+					previousArrow: 'coreSpriteLeftPaginationArrow' // link element
 				};
 				var followersList = null;
 				var followingList = null;
@@ -44,8 +49,115 @@ chrome.extension.sendMessage({}, function (response) {
 							break;
 						case 221: // closing square bracket
 							getFollowingThatAreNotFollowers();
+							break;
+						case 187: // = key
+							getPostMetaData(true);
+							break;
+						case 189: // - key
+							getAllPostMetaData();
+							break;
+						case 48: // 0 key
+							break;
+						case 57: // 9 key
+							break;
+						case 56: // 8 key
 					}
 				}, true);
+
+				function getAllPostMetaData() {
+					var posts = document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.postDiv);
+					var postsLength = posts.length;
+
+					// Open lightbox
+					posts[postsLength - 1].children[0].click();
+
+					// Delay start
+					window.setTimeout(function () {
+						var postsMeta = [];
+
+						new Promise((resolve, reject) => {
+							cycleThroughPosts(postsMeta, postsLength, postsLength - 1, resolve);
+						}).then((meta) => {
+							console.log(meta);
+							chrome.runtime.sendMessage({
+								type: 'copy',
+								text: meta.join('\n'),
+								success: 'The post meta data has been copied to the clipboard',
+								fail: 'Unable to copy your post meta data to the clipboard'
+							});
+						});
+
+					}, 200);
+				}
+
+				function cycleThroughPosts(postsMeta, postsLength, currentIndex, resolve) {
+					console.log(currentIndex);
+					if (currentIndex >= 0) {
+						asyncGetPostMetaData(postsMeta);
+
+						window.setTimeout(function () {
+
+							if (currentIndex !== 0) {
+								// Go to next photo
+								document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.previousArrow)[0].click();
+							}
+
+							cycleThroughPosts(postsMeta, postsLength, currentIndex - 1, resolve);
+						}, 200);
+					}
+					else {
+						//finish
+						resolve(postsMeta);
+					}
+				}
+
+				function asyncGetPostMetaData(array) {
+					window.setTimeout(function () {
+						array.push(getPostMetaData(false));
+					}, 200);
+				}
+
+				function getPostMetaData(copy) {
+					var data = [getPostDateTime(),
+						' ',
+					getPostUserMentions(),
+					getPostDescriptionHashTags(),
+					getPostLikesCount(),
+					getPostCommentsCount()];
+
+					if (copy) {
+						chrome.runtime.sendMessage({
+							type: 'copy',
+							text: data.join('\t'),
+							success: 'The post meta data has been copied to the clipboard',
+							fail: 'Unable to copy your post meta data to the clipboard'
+						});
+					}
+
+					return data.join('\t');
+				}
+
+				function getPostCommentsCount() {
+					return document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.postCommentListItem).length - 1; // Minus 1 to ignore the post description
+				}
+
+				function getPostLikesCount() {
+					return document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.postLikes)[0].children[0].innerText;
+				}
+
+				function getPostDescriptionHashTags() {
+					var text = document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.postCommentListItem)[0].innerText;
+					return text.match(/#\w+/g).join(', ');
+				}
+
+				function getPostUserMentions() {
+					var text = document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.postCommentListItem)[0].innerText;
+					return text.match(/@\w+/g) ? text.match(/@\w+/g)[0] : 'Other';
+				}
+
+				function getPostDateTime() {
+					return document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.postDateTime)[0].getAttribute('datetime');
+				}
 
 				function closeFollowerFollowingModal() {
 					document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.closeButton)[0].click();
@@ -176,7 +288,7 @@ chrome.extension.sendMessage({}, function (response) {
 				function loadList(resolve, count, previouslistItemsLength, attemptCount) {
 					var listItems = document.getElementsByClassName(INSTAGRAM_CLASS_NAMES.userListItem);
 					var listItemsLength = listItems.length;
-					
+
 					// Increase attempt count?
 					if (previouslistItemsLength === listItems.length) {
 						attemptCount += 1;
